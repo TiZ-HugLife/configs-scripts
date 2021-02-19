0ddb17b58f21eb3d71f43e1f48c22e77206e1ebb
Trent McPheron <twilightinzero@gmail.com>
2021-02-19 16:56:19 -0500

    So long and thanks for all the fish, do_rules().

--- devilspie2/00-base.lua
+++ devilspie2/00-base.lua
@@ -1,37 +1,4 @@
---[[ All-in one config script for simple matches.
-do_rules(
-    {app="etc", max="true", ...},
-    {class="etc", size={w=500, h=500}, ...},
-    ...
-)
-
-MATCH CRITERIA
-app = application name
-class = window class
-role = window role
-name = title bar contents
-min_width = window width is this or higher
-min_height = window height is this or higher
-type = window type, specified without the WINDOW_TYPE_ prefix and whatever case
-       you want. if nil, assumed to be normal. if *, all types.
-exc_name = exclude window name
-exc_type = exclude window type
-onlyfirst = only act on the first window matching the other criteria to open
-
-ACTION CRITERIA
-alpha = window opacity
-pos = a table with fields x and y for the desired position
-size = a table with fields w and h for the desired size
-wksp = target workspace starting from 1
-vport = target viewport starting from 1
-no_task = hide from tasklist
-no_page = hide from pager
-set_type = change the window's type. see type above.
-set_class = change the window's class.
-top, bottom, shade, max, min, undeco, stick, pin = do these to the window.
-fswin = borderless fullscreen window. aspect scales window to fill screen.
-run = run script on window with args followed by window id.
-]]
+-- Make simple matches easier to read and write with extra functions.
 
 local string_meta = getmetatable("")
 
@@ -109,57 +76,7 @@ undecorate = undecorate_window
 pin = pin_window
 stick = stick_window
 
--- Execute commands according to rules.
-function do_rules (...)
-    _, _, win_w, win_h = get_window_client_geometry()
-    for _, v in ipairs(arg) do
-        if
-            (not v.app or v.app:lower() == app_name:lower()) and
-            (not v.class or v.class:lower() == win_class:lower()) and
-            (not v.class2 or v.class2:lower() == class_instance:lower()) and
-            (not v.role or get_window_role():match(v.role)) and
-            (not v.name or get_window_name():match(v.name)) and
-            (not v.min_width or v.min_width >= win_w) and
-            (not v.min_height or v.min_height >= win_h) and
-            (not v.exc_name or v.exc_name ~= get_window_name()) and
-            (v.type == "*" or
-                "WINDOW_TYPE_"..string.upper(v.type or "normal") ==
-                get_window_type()) and
-            (not v.exc_type or "WINDOW_TYPE_"..string.upper(v.exc_type) ~=
-                get_window_type()) and
-            (not v.nth or is_nth(v))
-        then
-            debug_print("Window matched!")
-            if v.pos then set_window_position2(v.pos.x, v.pos.y) end
-            if v.size then set_window_size(v.size.w, v.size.h) end
-            if v.alpha then set_opacity(v.alpha) end
-            if v.wksp then
-                set_window_workspace(v.wksp)
-            end
-            if v.vport then set_viewport(v.vport) end
-            if v.no_task then set_skip_tasklist(true) end
-            if v.no_page then set_skip_pager(true) end
-            if v.top then make_always_on_top() end
-            if v.above then set_window_above() end
-            if v.below then set_window_below() end
-            if v.shade then shade() end
-            if v.max then maximize() end
-            if v.min then minimize() end
-            if v.undeco then undecorate_window() end
-            if v.pin then pin_window() end
-            if v.stick then stick_window() end
-            if v.set_type then set_type(v.set_type) end
-            --if v.set_class then
-                --set_window_property("WM_CLASS", v.set_class)
-            --end
-            if v.run then
-                os.execute(v.run .. " " .. get_window_xid())
-            end
-            if v.fswin then fswin(v.fsw_aspect) end
-        end
-    end
-end
-
+-- Create handy vars to reference in each script.
 win_name = (get_window_name() or "")
 app_name = (get_application_name() or "")
 win_class = (get_window_class() or "")
@@ -167,6 +84,7 @@ class_instance = (get_class_instance_name() or "")
 win_role = (get_window_role() or "")
 win_x, win_y, win_w, win_h = get_window_client_geometry()
 
+-- Debug info.
 debug_print("Name: " .. win_name)
 debug_print("App: " .. app_name)
 debug_print("Class: " .. win_class)
--- devilspie2/50-aura.lua
+++ devilspie2/50-aura.lua
@@ -38,31 +38,3 @@ if mode:is("homedesk") then
         pin_window()
     end
 end
-
-
---do_rules(
-    --{class2="cava", alpha=0.15, stick=true, no_task=true, no_page=true, pos={x=0, y=362}},
-    --{app="Xfce Terminal", size={w=702, h=471}},
-    --{app="Thunar", size={w=500, h=471}},
-    ---{app="Thunar", size={w=404, h=523}},
-    ---{app="Thunar", name="File Operation Progress", size={w=320, h=120}},
-    --{app="geany", size={w=702, h=929}},
-    ---{app="geany", size={w=790, h=1054}, pos={x=1130, y=26}},
-    ---{app="syncthing-gtk", size={w=660, h=500}},
-    --{app="gigolo", size={w=320, h=160}},
-    ---{app="emoji-keyboard", type="utility", undeco=true, alpha=0.80,
-    ---    pos={x=745, y=890}, size={w=430, h=190}},
-    --{name="Whisker Menu", type="dialog", size={w=420, h=480}},
-    --{app="Quod Libet", wksp=4, alpha=0.80},
-    ---{app="pegasus-frontend", run="xdotool mousemove 9999 9999"},
--- Specific Workspaces
-    ---{app="Evolution.bin", wksp=1},
-    ---{class="Ferdi", wksp=1, max=true},
-    ---{class="Caprine", wksp=1},
-    ---{class="discord", wksp=1},
-    ---{app="Spotify", wksp=2, max=true},
-    ---{app="Firefox", wksp=2},
-    ---{class="Microsoft Teams - Preview", wksp=1},
-    --{name="Microsoft Teams Notification", stick=true, no_page=true, no_task=true},
---    {}
---)
